c6e48ed12aec0526d58a1d7ebd148cc3492653e7
Jakob Dunlap <jakob.dunlap@outlook.com>
2025-12-22 15:17:49 -0600

    second commit

---
+++ src/Content.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+
+const Content = () => {
+  return (
+    <main>
+        <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              This is a paragraph lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </p>
+        </header>
+    </main>
+  )
+}
+
+export default Content
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+const Header = () => {
+    return (
+        <header>
+            <h1>
+                List element here
+            </h1>
+        </header>
+    )
+}
+
+export default Header
