c8fd2012f228698326ff0ba1e851b334d4471a5a
Jakob Dunlap <jakob.dunlap@outlook.com>
2026-01-26 21:26:00 -0600

    Added routes using React Router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
+import { createBrowserRouter, Route, createRoutesFromElements, RouterProvider } from 'react-router-dom';
 import './App.css';
-import Header from './Header';
-import Content from './Content';
-import Footer from './Footer';
+
+// legacy modules //
+//import Header from './Header';
+//import Content from './Content';
+//import Footer from './Footer';
+
+// modules //
+import Home from './pages/Home';
+import Form from './pages/Form';
+
+// layouts //
+import RootLayout from './layouts/RootLayout';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<Home />} />
+      <Route path="form" element={<Form />} />
+    </Route>
+  )
+)
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Content />
-      <Footer />
-
-    </div>
+      <RouterProvider router={router} />
   );
 }
 
---
+++ src/pages/Form.js
@@ -0,0 +1,9 @@
+
+export default function Form() {
+    return (
+        <div className="form">
+            <h3>Insert form here</h3>
+            <form></form>
+        </div>
+    )
+}
